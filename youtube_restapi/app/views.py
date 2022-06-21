fc36359f2aee5450b13e74aeed15c602430de025
Pavan Kumar Bellam <60264606+Pavan-Bellam@users.noreply.github.com>
2022-06-21 10:19:48 +0530

    Update views.py
    add a get function

--- youtube_restapi/app/views.py
+++ youtube_restapi/app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from app.models import Videos
+from app.serializer import Serializer
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+@api_view(["GET"])
+def get_videos(request):
+    paginator = PageNumberPagination()
+    query_set = Videos.objects.all()
+    context = paginator.paginate_queryset(query_set, request)
+    serializer = Serializer(context, many=True)
+
+    return paginator.get_paginated_response(serializer.data)
