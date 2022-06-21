94cf19e556637608bc3d806940a454136a8f9975
Pavan Kumar Bellam <60264606+Pavan-Bellam@users.noreply.github.com>
2022-06-21 10:18:09 +0530

    Update models.py
    Creating a model to save data received from youtube api

--- youtube_restapi/app/models.py
+++ youtube_restapi/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+
+class Videos(models.Model):
+    title = models.CharField( max_length=255)
+    desc = models.TextField()
+    publishing_date = models.DateTimeField()
+    url_for_thumnail = models.URLField(max_length=255)
+    link = models.URLField(max_length=255)
+
+    class Meta:
+        ordering = ["-publishing_date"]  # To sort in reverse chronological order
+    def __str__(self):
+        return self.title
